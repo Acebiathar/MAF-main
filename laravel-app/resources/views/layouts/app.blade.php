e27cdaa89266ff216b2a282e4072a01670de84e2
lamulahnaddamba84-oss <lamulahnaddamba84@gmail.com>
2026-04-20 09:26:31 +0300

    Update app.blade.php

--- laravel-app/resources/views/layouts/app.blade.php
+++ laravel-app/resources/views/layouts/app.blade.php
@@ -9,6 +9,13 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+=======
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+>>>>>>> Stashed changes
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <style>
     .footer-gradient {
